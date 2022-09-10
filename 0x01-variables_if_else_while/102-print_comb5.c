bc3e9f8d98eea7c35f8b9129791433f4d5e08dd2
faustyTech <faustyluv5@gmail.com>
2022-09-10 00:13:26 -0700

    0x01 variables task 10-12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - print possible combination of two 2-digit
+ *
+ * Return: returns zero at the end
+ */
+int main(void)
+{
+int i, p;
+for (p = 0; p < 100; j++)
+{
+if (p > i)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+if (i != 98)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}

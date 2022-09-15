be728c886edacd5c5f6ff414a3a742bda432440a
faustyTech <faustyluv5@gmail.com>
2022-09-15 15:45:58 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+print_square -prints squares
+*@size: parameter
+*Return: returns nothing
+*/
+
+void print_square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)

40cea88de3ed69dd65d6858c94615a68a34f7bf3
faustyTech <faustyluv5@gmail.com>
2022-10-13 15:42:49 -0700

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -6,15 +6,24 @@
  * @n: number of parameters
  * Return: sum of all parameters
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0;
-	unsigned int i;
-	va_list valist;
 
-	va_start(valist, n);
+	va_list args;
+	int sum;
+	unsigned int i;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg(valist, int);
-	va_end(valist);
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+
 	return (sum);
 }

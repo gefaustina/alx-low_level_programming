ccc64560aba476cd1d1f165522799c1a18af7806
faustyTech <faustyluv5@gmail.com>
2022-10-20 19:43:17 -0700

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,16 @@
-section .text
-   global main
+section .data
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
+
+	section .text
+	extern printf
+	global main
 
 main:
-   push rbp
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-   mov rdi,fmt
-   mov rsi,msg
-   mov rax,0
-   call printf
-
-   pop rbp
-
-   mov rax,
-   ret
-
-section .data
-   msg: db "Hello, Holberton", 0
-   fmt: db "%s", 10, 0
+	mov eax, 0
+	ret
